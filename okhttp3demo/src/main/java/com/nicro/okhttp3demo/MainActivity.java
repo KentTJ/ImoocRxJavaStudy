d2db3fc571861e412f5d755009158f9ddd642c00
rongwenzhao <rong_wz@126.com>
2018-07-14 18:06:46 +0800

    学习文档。

--- okhttp3demo/src/main/java/com/nicro/okhttp3demo/MainActivity.java
+++ okhttp3demo/src/main/java/com/nicro/okhttp3demo/MainActivity.java
@@ -1,7 +1,16 @@
 package com.nicro.okhttp3demo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,7 +19,31 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+    }
 
+    private static final String URL_CONTENT = "https://www.baidu.com/";
 
+    public void doOkHttp(View view) {
+        OkHttpClient okHttpClient = new OkHttpClient.Builder()
+                .retryOnConnectionFailure(true)  //连接失败后进行重连
+                .build();
+
+        final Request request = new Request.Builder()
+                .url(URL_CONTENT)
+                .build();
+
+        //异步方式请求网络
+        okHttpClient.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                //请求失败
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                //请求成功,对response进行处理
+
+            }
+        });
     }
 }
