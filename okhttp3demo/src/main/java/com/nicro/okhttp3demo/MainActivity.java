d48d4e2866df5aad5ee9fc3dd2c35d0f4c74b3cc
rongwenzhao <rong_wz@126.com>
2018-07-15 19:12:08 +0800

    okhttp缓存部分提交。

--- okhttp3demo/src/main/java/com/nicro/okhttp3demo/MainActivity.java
+++ okhttp3demo/src/main/java/com/nicro/okhttp3demo/MainActivity.java
@@ -1,11 +1,15 @@
 package com.nicro.okhttp3demo;
 
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 
+import java.io.File;
 import java.io.IOException;
 
+import okhttp3.Cache;
 import okhttp3.Call;
 import okhttp3.Callback;
 import okhttp3.OkHttpClient;
@@ -21,13 +25,27 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    private static String cacheDirectory = Environment.getExternalStorageDirectory() + "/okttpcaches"; // 设置缓存文件路径
     private static final String URL_CONTENT = "https://www.baidu.com/";
+    OkHttpClient okHttpClient = new OkHttpClient.Builder()
+            .cache(new Cache(new File(cacheDirectory), 24 * 1024 * 1024))
+            .retryOnConnectionFailure(true)  //连接失败后进行重连
+            .build();
+
+    public void syncRequest() {
+        final Request request = new Request.Builder()
+                .url(URL_CONTENT)
+                .build();
+        Call call = okHttpClient.newCall(request);
+        try {
+            Response response = call.execute();
+            System.out.println(response.body());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
     public void doOkHttp(View view) {
-        OkHttpClient okHttpClient = new OkHttpClient.Builder()
-                .retryOnConnectionFailure(true)  //连接失败后进行重连
-                .build();
-
         final Request request = new Request.Builder()
                 .url(URL_CONTENT)
                 .build();
@@ -42,6 +60,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onResponse(Call call, Response response) throws IOException {
                 //请求成功,对response进行处理
+                Log.d("rwz", "return ok and response = " + response);
 
             }
         });
